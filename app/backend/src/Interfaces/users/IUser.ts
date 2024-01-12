e1f6b68d80ad272f78a53414f0f7b7b8f82f11c1
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-01-12 15:40:27 -0300

    migrations

---
+++ app/backend/src/Interfaces/users/IUser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  id: number;
+  userName: string;
+  role: string;
+  email: string;
+  password: string;
+
+}
---
+++ app/backend/src/database/migrations/03-create-users-table.ts
@@ -0,0 +1,37 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import { IUser } from '../../Interfaces/users/IUser';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique:true,
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+};
