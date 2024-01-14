fcf9fa7f7d2ceecb591d0fc349af0cb0986ffa21
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-01-14 17:53:45 -0300

    feat: ServiceLogin

--- app/backend/src/services/ServiceLogin.ts
+++ app/backend/src/services/ServiceLogin.ts
@@ -2,9 +2,8 @@ import * as bcrypt from 'bcryptjs';
 import { Attributes, Model, ModelCtor, WhereAttributeHashValue } from 'sequelize';
 // import SequelizeTeam from '../database/models/SequelizeTeam';
 import { ServiceMessage, ServiceResponse } from '../Interfaces/ServiceResponse';
-import { ID } from '../Interfaces/ICRUDModel';
 import JWT from '../utils/JWT';
-import { IToken, ILogin } from '../Interfaces/IEntities';
+import { IToken, ILogin, IUser } from '../Interfaces/IEntities';
 import CustomError from '../utils/CustomError';
 
 export default class LoginService<T extends Model> {
@@ -13,13 +12,15 @@ export default class LoginService<T extends Model> {
     this.model = model;
   }
 
-  public async login(login: ILogin): Promise<ServiceMessage | IToken> {
-    const { email, password } = login;
+  public async login(login: ILogin): Promise<ServiceResponse<ServiceMessage | IToken>> {
     const user = await this.model
-      .findOne({ where: { email } as WhereAttributeHashValue<Attributes<T>[string]> });
-    if (!user || !bcrypt.compareSync(password, user.toJSON().password)) {
+      .findOne({ where: { email: login.email } as WhereAttributeHashValue<Attributes<T>[string]> });
+    if (!user || !bcrypt.compareSync(login.password, user.toJSON().password)) {
       throw new CustomError('Ivalid email or password', 401);
     }
+    const { email, role } = user.toJSON() as IUser;
+    const token = this.jwtService.sign({ email, role });
+    return { status: 'SUCCESSFUL', data: { token } };
   }
 }
 
