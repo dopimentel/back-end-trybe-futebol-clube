c1e1a8c9f237a1d6c766860f5562d860e808078b
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-01-15 18:04:39 -0300

    GET/matches - partial

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
+import { Request, Router, Response } from 'express';
+import SequelizeMatch from '../database/models/SequelizeMatch';
+import ReaderModel from '../models/ModelReader';
+import ReaderService from '../services/ServiceReader';
+import ReaderController from '../controllers/ControllerReader';
+
+const matchesReaderModel = new ReaderModel(SequelizeMatch);
+const matchesReaderService = new ReaderService(matchesReaderModel, 'Match');
+const matchesReaderController = new ReaderController(matchesReaderService);
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-  res.send('GET /matches');
-});
+router.get('/', (req: Request, res: Response) => matchesReaderController.getAll(req, res));
 
 export default router;
